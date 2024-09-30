f18df5ab13117e9b5157a74d90abc31f7df40850
Lázaro Baggi <baggitech@gmail.com>
2024-09-30 00:31:02 -0300

    Update poptag_app_db.sql
    atualização manual do banco de dados

--- database/poptag_app_db.sql
+++ database/poptag_app_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/09/2024 às 16:52
+-- Tempo de geração: 30/09/2024 às 05:28
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -34,6 +34,13 @@ CREATE TABLE `blocks` (
   `block_orderliness` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `blocks`
+--
+
+INSERT INTO `blocks` (`block_id`, `link_id`, `block_type`, `block_orderliness`) VALUES
+(1, 1, 'avatar', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +70,13 @@ CREATE TABLE `block_avatar` (
   `avatar_created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `avatar_updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Despejando dados para a tabela `block_avatar`
+--
+
+INSERT INTO `block_avatar` (`avatar_id`, `link_id`, `avatar_image`, `avatar_image_alt`, `avatar_location_url`, `avatar_target_link`, `avatar_height`, `avatar_width`, `avatar_border_radius`, `avatar_border_shadow_offset_x`, `avatar_border_shadow_offset_y`, `avatar_border_shadow_blur`, `avatar_border_shadow_spread`, `avatar_border_shadow_color`, `avatar_border_width`, `avatar_border_style`, `avatar_border_color`, `avatar_object_fit`, `avatar_is_enabled`, `avatar_created_at`, `avatar_updated_at`) VALUES
+(1, 1, 'gxzqu2k3.jpeg', NULL, NULL, NULL, '125px', '125px', 'round', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, '2024-09-29 19:48:45', '2024-09-30 03:17:06');
 
 -- --------------------------------------------------------
 
@@ -187,8 +201,7 @@ CREATE TABLE `links` (
 --
 
 INSERT INTO `links` (`link_id`, `user_id`, `link_name`, `link_type`, `link_settings`, `link_is_enabled`, `link_created_at`, `link_updated_at`) VALUES
-(8, 1, '3gyeuj4vcc', 'biolink', NULL, 1, '2024-09-26 14:55:45', '2024-09-27 14:33:29'),
-(9, 1, 'rv7yupbass', 'biolink', NULL, 1, '2024-09-27 12:52:34', '2024-09-27 14:46:26');
+(1, 1, 'gxzqu2k3', 'biolink', NULL, 1, '2024-09-29 18:28:58', '2024-09-29 18:28:58');
 
 -- --------------------------------------------------------
 
@@ -214,8 +227,7 @@ CREATE TABLE `link_background` (
 --
 
 INSERT INTO `link_background` (`background_id`, `link_id`, `background_type`, `background_color_code`, `background_color_one`, `background_color_two`, `background_to_direction`, `background_image`, `background_created_at`, `background_updated_at`) VALUES
-(8, 8, NULL, NULL, NULL, NULL, NULL, NULL, '2024-09-26 14:55:45', '2024-09-26 14:55:45'),
-(9, 9, 'gradient', 'two', '#ffb418', '#f73131', 'to bottom', NULL, '2024-09-27 12:52:34', '2024-09-27 14:43:45');
+(1, 1, 'gradient', 'eight', '#44A08D', '#093637', 'to bottom', NULL, '2024-09-29 18:28:58', '2024-09-29 19:48:54');
 
 -- --------------------------------------------------------
 
@@ -240,8 +252,7 @@ CREATE TABLE `link_domain` (
 --
 
 INSERT INTO `link_domain` (`domain_id`, `link_id`, `domain_type`, `domain_name`, `domain_host`, `domain_custom_index_url`, `domain_custom_not_found_url`, `domain_created_at`, `domain_updated_at`) VALUES
-(8, 8, NULL, 'poptag.app', NULL, NULL, NULL, '2024-09-26 14:55:45', '2024-09-27 14:33:29'),
-(9, 9, NULL, 'poptag.app', NULL, NULL, NULL, '2024-09-27 12:52:34', '2024-09-27 14:14:58');
+(1, 1, NULL, 'poptag.app', NULL, NULL, NULL, '2024-09-29 18:28:58', '2024-09-29 18:28:58');
 
 -- --------------------------------------------------------
 
@@ -266,8 +277,7 @@ CREATE TABLE `link_fonts` (
 --
 
 INSERT INTO `link_fonts` (`font_id`, `link_id`, `font_one`, `font_two`, `font_three`, `font_size`, `font_color`, `font_created_at`, `font_updated_at`) VALUES
-(8, 8, NULL, NULL, NULL, NULL, NULL, '2024-09-26 14:55:45', '2024-09-26 14:55:45'),
-(9, 9, NULL, NULL, NULL, NULL, NULL, '2024-09-27 12:52:34', '2024-09-27 12:52:34');
+(1, 1, NULL, NULL, NULL, NULL, NULL, '2024-09-29 18:28:58', '2024-09-29 18:28:58');
 
 -- --------------------------------------------------------
 
@@ -292,8 +302,7 @@ CREATE TABLE `link_seo` (
 --
 
 INSERT INTO `link_seo` (`seo_id`, `link_id`, `seo_robots`, `seo_title`, `seo_meta_description`, `seo_meta_keywords`, `seo_favicon`, `seo_created_at`, `seo_updated_at`) VALUES
-(8, 8, NULL, NULL, NULL, NULL, NULL, '2024-09-26 14:55:45', '2024-09-26 14:55:45'),
-(9, 9, NULL, 'Titulo aqui', '', '', NULL, '2024-09-27 12:52:34', '2024-09-27 13:51:43');
+(1, 1, NULL, NULL, NULL, NULL, NULL, '2024-09-29 18:28:58', '2024-09-29 18:28:58');
 
 -- --------------------------------------------------------
 
@@ -616,8 +625,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `email`, `password`, `name`, `billing`, `api_key`, `token`, `twofa_secret`, `anti_phishing_code`, `one_time_login_code`, `pending_email`, `email_activation_code`, `lost_password_code`, `type`, `verified`, `plan_id`, `plan_expiration_date`, `plan_settings`, `plan_trial_done`, `plan_expiry_reminder`, `payment_subscription_id`, `payment_processor`, `payment_total_amount`, `payment_currency`, `referral_key`, `referred_by`, `referred_by_has_converted`, `language`, `timezone`, `ip`, `country`, `last_activity`, `last_user_agent`, `total_logins`, `user_deletion_reminder`, `source`, `level`, `code_verify`) VALUES
-(1, 'admin@gmail.com', '9b267f64fbfecc6fe57a4e51ebff29ecd3b84c10863a0847279cba076f776e3c', 'AltumCode', NULL, 'cc4c27ae1085ad965050c668c1f7bb7d', '7b04df736346fc8fc03c5c7607e932239033dfd5e4d10bb3c37e860ee87828df', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, 'custom', '2030-01-01 12:00:00', '{\"additional_global_domains\":true,\"custom_url\":true,\"deep_links\":true,\"no_ads\":true,\"removable_branding\":true,\"custom_branding\":true,\"custom_colored_links\":true,\"statistics\":true,\"qr_is_enabled\":true,\"custom_backgrounds\":true,\"verified\":true,\"temporary_url_is_enabled\":true,\"seo\":true,\"utm\":true,\"fonts\":true,\"password\":true,\"sensitive_content\":true,\"leap_link\":true,\"api_is_enabled\":true,\"affiliate_is_enabled\":true,\"dofollow_is_enabled\":true,\"biolink_blocks_limit\":-1,\"projects_limit\":-1,\"pixels_limit\":-1,\"biolinks_limit\":-1,\"links_limit\":-1,\"domains_limit\":-1,\"track_links_retention\":-1,\"enabled_biolink_blocks\":{\"link\":true,\"heading\":true,\"paragraph\":true,\"avatar\":true,\"image\":true,\"socials\":true,\"mail\":true,\"soundcloud\":true,\"spotify\":true,\"youtube\":true,\"twitch\":true,\"vimeo\":true,\"tiktok\":true,\"applemusic\":true,\"tidal\":true,\"anchor\":true,\"twitter_tweet\":true,\"instagram_media\":true,\"rss_feed\":true,\"custom_html\":true,\"vcard\":true,\"image_grid\":true,\"divider\":true,\"faq\":true,\"discord\":true,\"facebook\":true,\"reddit\":true,\"audio\":true,\"video\":true,\"file\":true,\"countdown\":true,\"cta\":true,\"external_item\":true,\"share\":true,\"youtube_feed\":true}}', 0, 0, NULL, NULL, NULL, NULL, '96f3359c8a43dda4b9ad9bda57f1197f', NULL, 0, 'english', 'UTC', '::1', NULL, '2022-12-05 03:19:27', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 2, 0, 'direct', 3, '604532'),
-(34, 'baggitech@gmail.com', '9b267f64fbfecc6fe57a4e51ebff29ecd3b84c10863a0847279cba076f776e3c', 'Lazaro', NULL, NULL, 'f6e3db2da5e51590aa91a13e0929813460f097e2246cee91c72424ea1e937ca4', NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, '', NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, 0, 'english', 'UTC', NULL, NULL, NULL, NULL, 0, 0, 'direct', 1, '713845');
+(1, 'admin@gmail.com', '9b267f64fbfecc6fe57a4e51ebff29ecd3b84c10863a0847279cba076f776e3c', 'AltumCode', NULL, 'cc4c27ae1085ad965050c668c1f7bb7d', '3354f2582229e32a4dd6d2cbbfc0d389ad8188a572a04923a76580c07fa2641e', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, 'custom', '2030-01-01 12:00:00', '{\"additional_global_domains\":true,\"custom_url\":true,\"deep_links\":true,\"no_ads\":true,\"removable_branding\":true,\"custom_branding\":true,\"custom_colored_links\":true,\"statistics\":true,\"qr_is_enabled\":true,\"custom_backgrounds\":true,\"verified\":true,\"temporary_url_is_enabled\":true,\"seo\":true,\"utm\":true,\"fonts\":true,\"password\":true,\"sensitive_content\":true,\"leap_link\":true,\"api_is_enabled\":true,\"affiliate_is_enabled\":true,\"dofollow_is_enabled\":true,\"biolink_blocks_limit\":-1,\"projects_limit\":-1,\"pixels_limit\":-1,\"biolinks_limit\":-1,\"links_limit\":-1,\"domains_limit\":-1,\"track_links_retention\":-1,\"enabled_biolink_blocks\":{\"link\":true,\"heading\":true,\"paragraph\":true,\"avatar\":true,\"image\":true,\"socials\":true,\"mail\":true,\"soundcloud\":true,\"spotify\":true,\"youtube\":true,\"twitch\":true,\"vimeo\":true,\"tiktok\":true,\"applemusic\":true,\"tidal\":true,\"anchor\":true,\"twitter_tweet\":true,\"instagram_media\":true,\"rss_feed\":true,\"custom_html\":true,\"vcard\":true,\"image_grid\":true,\"divider\":true,\"faq\":true,\"discord\":true,\"facebook\":true,\"reddit\":true,\"audio\":true,\"video\":true,\"file\":true,\"countdown\":true,\"cta\":true,\"external_item\":true,\"share\":true,\"youtube_feed\":true}}', 0, 0, NULL, NULL, NULL, NULL, '96f3359c8a43dda4b9ad9bda57f1197f', NULL, 0, 'english', 'UTC', '::1', NULL, '2022-12-05 03:19:27', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 2, 0, 'direct', 3, '604532'),
+(34, 'baggitech@gmail.com', '9b267f64fbfecc6fe57a4e51ebff29ecd3b84c10863a0847279cba076f776e3c', 'Lazaro', NULL, NULL, '6821eaf1eff260a860692cfee009dabf1508cffa1470c0ac615607b06f1211db', NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, '', NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, 0, 'english', 'UTC', NULL, NULL, NULL, NULL, 0, 0, 'direct', 1, '713845');
 
 --
 -- Índices para tabelas despejadas
@@ -733,13 +742,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `blocks`
 --
 ALTER TABLE `blocks`
-  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `block_avatar`
 --
 ALTER TABLE `block_avatar`
-  MODIFY `avatar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `avatar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `languages`
@@ -751,31 +760,31 @@ ALTER TABLE `languages`
 -- AUTO_INCREMENT de tabela `links`
 --
 ALTER TABLE `links`
-  MODIFY `link_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `link_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `link_background`
 --
 ALTER TABLE `link_background`
-  MODIFY `background_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `background_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `link_domain`
 --
 ALTER TABLE `link_domain`
-  MODIFY `domain_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `domain_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `link_fonts`
 --
 ALTER TABLE `link_fonts`
-  MODIFY `font_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `font_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `link_seo`
 --
 ALTER TABLE `link_seo`
-  MODIFY `seo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `seo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `link_themes`
